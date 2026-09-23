f7870173548a313ac98b311dcb6879a1c6f9e075
Sean Alunni <sean.alunni@gbsoftware.it>
2026-09-23 17:44:28 +0200

    Aggiunte sorgenti

--- package/vscode-lang-cil/src/index.ts
+++ package/vscode-lang-cil/src/index.ts
@@ -4,6 +4,7 @@ import * as vscode from "vscode";
 // TODO: https://vshaxe.github.io/vscode-extern/vscode/DocumentSemanticTokensProvider.html
 // TODO: https://github.com/EvgeniyPeshkov/syntax-highlighter/blob/master/src/extension.ts
 // TODO: https://github.com/AlecGhost/tree-sitter-vscode/blob/master/src/extension.ts
+// TODO: https://github.com/microsoft/vscode-extension-samples/blob/main/semantic-tokens-sample/src/extension.ts
 
 export function activate(ctx: vscode.ExtensionContext) {
 	ctx;
